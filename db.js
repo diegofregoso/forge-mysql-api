816e969cd8c4295addbf1b3b725578583559fa0f
diegofregoso <imdiegofregoso@gmail.com>
2026-02-12 11:39:52 -0600

    Eliminación de innecesarios

--- db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-});
